53864870bcd37a556be7907293047924105eb669
Arushi Katyal <katy.arushi@gmail.com>
2021-09-21 22:28:49 -0400

    implement tweet char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,18 @@
 $(document).ready(() => {
-  console.log("loaded")
-  $('#tweet-text').keypress(() => {
-    console.log(this)
+  $('#tweet-text').on('input', function (event) {
+    const maxChars = 140;
+    let textArea = event.target;
+    let count = $(textArea).val().length;
+    let charsLeft = (maxChars - count);
+    let counter = $(textArea).next().find('output'); // classes are re-used, could have a side effect
+    counter.text(charsLeft)
+    if (charsLeft < 0) { // for making the counter red
+      counter.addClass("warningText") // better practice to add a class rather than in line css style
+    } else {
+      counter.removeClass("warningText") 
+    }
   });
 });
 
+// find() goes inward, children
+// closest() goes upward the tree
