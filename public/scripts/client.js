54b8985ac3034ea78e02837447a5a79706cdf08b
Arushi Katyal <katy.arushi@gmail.com>
2021-09-27 01:14:02 -0400

    implement renderTweets function in client.js to render tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,12 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(() => {
-  const renderTweets = function(tweets) {
-
-  };
   
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+
   const createTweetElement = function(tweet) {
     let $tweet = `
     <article class="all-tweets-border">
@@ -32,25 +30,5 @@ $(document).ready(() => {
 
     return $tweet;
   };
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
-  $('#tweet-text').on('input', function (event) {
+  $('#tweet-text').on('input', function(event) {
     const maxChars = 140;
     let textArea = event.target;
     let count = $(textArea).val().length;
     let charsLeft = (maxChars - count);
     let counter = $(textArea).next().find('output'); // classes are re-used, could have a side effect
-    counter.text(charsLeft)
+    counter.text(charsLeft);
     if (charsLeft < 0) { // for making the counter red
-      counter.addClass("warningText") // better practice to add a class rather than in line css style
+      counter.addClass("warningText"); // better practice to add a class rather than in line css style
     } else {
-      counter.removeClass("warningText") 
+      counter.removeClass("warningText");
     }
   });
 });
